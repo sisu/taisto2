52372b6755094a3792e30023b39bab09f27bbc42
Aleksi Hartikainen <aleksi@moukari.(none)>
2010-04-01 14:35:33 +0300

    soundtest.cpp lisätty

---
+++ soundtest.cpp
@@ -0,0 +1,33 @@
+#include "music.hpp"
+#include<SDL/SDL.h>
+#include<iostream>
+
+
+int main(int argc, char* argv[])
+{
+    SDL_Init( SDL_INIT_AUDIO | SDL_INIT_TIMER);
+    initMusic();
+	SDL_PauseAudio(0);
+    //sounds.push_back(Sound(EXPLOSION, .5));
+    //sounds.push_back(Sound(SHOTGUNSOUND, .5));
+    //sounds.push_back(Sound(MACHINEGUNSOUND, .5));
+    sounds.push_back(Sound(FLAMESOUND, .5));
+	bool done=0;
+    int time = 0;
+	while(!done) {
+        time++;
+        std::cout<<time*50<<"\n";
+        if(time*50>500)
+        {
+            return 0;
+        }
+		SDL_Delay(50);
+		SDL_Event e;
+		while(SDL_PollEvent(&e)) {
+			if (e.type==SDL_QUIT) done=1;
+		}
+	}
+	SDL_PauseAudio(1);
+
+    SDL_Delay(1000*5);
+}
